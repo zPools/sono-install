c3f9abb85d15c8599ddeeb886a500758f4fb5a67
Mike.zPools <zpoolsde@gmail.com>
2018-08-09 13:06:00 +0200

    sonoa update

--- sono-install.sh
+++ sono-install.sh
@@ -22,14 +22,14 @@ echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 clear && echo Downloading the wallet from source and compile it ourself && sleep 3
 
 #Building the wallet (Get yourself some coffee, this may takes some time)
-git clone https://github.com/zPools/sonoa.git ~/sono
+git clone https://github.com/zPools/sonoa.git ~/sonoa
 
 #Run the dependencies, to be on the save side
-cd ~/sono
-sudo chmod +x install-dependencies.sh
-./install-dependencies.sh
+# cd ~/sono
+# sudo chmod +x install-dependencies.sh
+# ./install-dependencies.sh
 
-#Tripple verify correct Barkley
+#Verify correct Barkley
 sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install -y libdb4.8-dev libdb4.8++-dev 
@@ -43,32 +43,35 @@ make -f makefile.unix
 clear && echo Wallet is compiled. We now create the standard configuration && sleep 3
 
 #Building the config
-mkdir ~/.SONO/
-touch ~/.SONO/SONO.conf
-ex -sc '1i|addnode=multi.zPools.de' -cx ~/.SONO/SONO.conf
-ex -sc '1i|addnode=zPools.de' -cx ~/.SONO/SONO.conf
-ex -sc '1i|#masternodeprivkey=”Private Key”' -cx ~/.SONO/SONO.conf
-ex -sc '1i|#masternodeaddr=”Change it to server IP:Port (26855)”' -cx ~/.SONO/SONO.conf
-ex -sc '1i|#masternode=1' -cx ~/.SONO/SONO.conf
-ex -sc '1i|port=26855' -cx ~/.SONO/SONO.conf
-ex -sc '1i|listen=1' -cx ~/.SONO/SONO.conf
-ex -sc '1i|server=1' -cx ~/.SONO/SONO.conf
-ex -sc '1i|daemon=1' -cx ~/.SONO/SONO.conf
-ex -sc '1i|rpcpassword=MakeItRandom' -cx ~/.SONO/SONO.conf
-ex -sc '1i|rpcuser=NeedToSoundCool' -cx ~/.SONO/SONO.conf
+mkdir ~/.SONOv2/
+touch ~/.SONOv2/sono.conf
+ex -sc '1i|addnode=seed1.projectsono.io' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|addnode=seed2.projectsono.io' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|addnode=seed3.projectsono.io' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|addnode=gfx-world.org' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|#masternodeprivkey=”Private Key”' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|#masternodeaddr=”Change it to server IP:Port (32000)”' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|#masternode=1' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|port=32000' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|listen=1' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|server=1' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|daemon=1' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|rpcpassword=MakeItRandom' -cx ~/.SONOv2/sono.conf
+ex -sc '1i|rpcuser=NeedToSoundCool' -cx ~/.SONOv2/sono.conf
 
 clear && echo Configuration is set, we now make it smoother to work with && sleep 3
 
 #Make it smooth to use
-sudo cp ~/sono/src/SONOd /usr/bin
+sudo cp ~/sono/src/sonod /usr/bin
 
 #Set the wallet to “autostart”
 crontab -l > mycron
-echo "@reboot SONOd" >> mycron
+echo "@reboot sonod" >> mycron
 crontab mycron
 rm mycron
 
 #Starting the wallet
-SONOd
+sonod
 
-clear && echo The wallet is now running. Now the server builds the chain. You now need to edit the config file.
+clear && echo The wallet should run now and builds the chain. You now need to edit the config file and restart. && echo //Mike
+
